b91cb41bdd5a05a6a8110c3fff1f5b6ebcb92b4d
Jenya Veprytskyi <jenya@nwt-llc.com>
2023-09-14 13:43:59 -0400

    contact form fixes

--- src/pages/main/contacts/Contacts.js
+++ src/pages/main/contacts/Contacts.js
@@ -26,10 +26,10 @@ const Contacts = () => {
               </div>
               <div className="contact-text">
                 <a
-                  href="https://www.google.com/maps/place/34%C2%B005'13.4%22N+84%C2%B000'00.9%22W/@34.0870421,-84.0008002,19z/data=!3m1!4b1!4m5!3m4!1s0x0:0xc158c4fe39ca4cf2!8m2!3d34.087041!4d-84.000253"
+                  href="https://www.google.com/maps/place/34%C2%B005'15.3%22N+84%C2%B000'03.3%22W/@34.0875831,-84.0015607,232m/data=!3m2!1e3!4b1!4m4!3m3!8m2!3d34.087582!4d-84.000917?entry=ttu"
                   target="blank"
                 >
-                  2400 Satellite Blvd NE, Buford, GA 30518, Suite E
+                  2400 Satellite Blvd NE, Buford, GA 30518, <b>Suite E</b>
                 </a>
               </div>
             </div>
@@ -74,12 +74,14 @@ const Contacts = () => {
         </div>
       </div>
       <iframe
-        src="https://www.google.com/maps/embed?pb=!1m14!1m8!1m3!1d509.30527885825063!2d-84.001134!3d34.087467!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x0%3A0x75a68ad86afb1573!2sNew%20World%20Telecom%20LLC!5e1!3m2!1suk!2sus!4v1675354983583!5m2!1suk!2sus"
         className="map"
-        title="google map"
-        allowFullScreen
-        loading="lazy"
-        referrerPolicy="no-referrer-when-downgrade"
+        width="100%"
+        height="600"
+        frameborder="0"
+        scrolling="no"
+        marginheight="0"
+        marginwidth="0"
+        src="https://maps.google.com/maps?width=100%25&amp;height=600&amp;hl=en&amp;q=2400%20Satellite%20Blvd%20Suite%20E,%20Buford,%20GA%2030518+(New%20World%20Telecom%20LLC)&amp;t=k&amp;z=18&amp;ie=UTF8&amp;iwloc=B&amp;output=embed"
       ></iframe>
 
       <div className="office-image">
