45056316d31d41139d79aa64ae864c2f69716921
Jenya Veprytskyi <jenya@nwt-llc.com>
2023-09-20 12:37:30 -0400

    add transition on dropdown

--- src/pages/training/trainingProgram/trainingProgramDropdown/TrainingProgramDropdown.js
+++ src/pages/training/trainingProgram/trainingProgramDropdown/TrainingProgramDropdown.js
@@ -11,21 +11,19 @@ const TrainingProgramDropdown = (props) => {
         <h3>{props.label}</h3>
       </div>
       <div className="dropdown-button" onClick={() => setIsOpen(!isOpen)}>
-        <p className="training-button">
+        <div className="training-button">
           <p>Read more</p>
           <div className={`arrow-down ${isOpen ? "arrow-down-closed" : ""}`}>
             <ArrowDownIcon />
           </div>
-        </p>
-      </div>
-      {isOpen && (
-        <div className="dropdown-content">
-          <img src={props.img} alt="training illustration" />
-          <p>{props.text1}</p>
-          <p>{props.text2}</p>
-          <p>{props.text3}</p>
         </div>
-      )}
+      </div>
+      <div className={`dropdown-content ${isOpen ? "dropdown-content-close" : ""}`}>
+        <img src={props.img} alt="training illustration" />
+        <p>{props.text1}</p>
+        <p>{props.text2}</p>
+        <p>{props.text3}</p>
+      </div>
     </div>
   );
 };
