df6af45bab47b11080d7b9006bdb0439244eed4c
Jenya Veprytskyi <jenya@nwt-llc.com>
2023-10-02 08:09:31 -0400

    fixed content text

--- src/pages/training/trainingProgram/TrainingProgram.js
+++ src/pages/training/trainingProgram/TrainingProgram.js
@@ -4,16 +4,16 @@ import Wer from "./TrainingProgram.json";
 import "./TrainingProgram.scss";
 
 const TrainingProgram = () => {
-  return (
-    <div className="training-program-wrapper">
-      <div className="training-program-content">
-        <h1>Coaxial Splicing treaing program</h1>
-        {Wer.map((item) => (
-          <TrainingProgramDropdown key={item.id} img={item.img} label={item.label} text1={item.text1} text2={item.text2} text3={item.text3} />
-        ))}
-      </div>
-    </div>
-  );
+    return (
+        <div className="training-program-wrapper">
+            <div className="training-program-content">
+                <h1>Coaxial Splicing training program</h1>
+                {Wer.map((item) => (
+                    <TrainingProgramDropdown key={item.id} img={item.img} label={item.label} text1={item.text1} text2={item.text2} text3={item.text3} />
+                ))}
+            </div>
+        </div>
+    );
 };
 
 export default TrainingProgram;
