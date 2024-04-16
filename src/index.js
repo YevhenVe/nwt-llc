750a6353f6e8cb5e612b4478ffd08d6bbf456925
Yevhen Veprytskyi <jenya@nwt-llc.com>
2024-04-16 09:37:52 -0400

    changed get hiring, added google analitics

--- src/index.js
+++ src/index.js
@@ -1,9 +1,12 @@
 import React, { Suspense } from "react";
 import ReactDOM from "react-dom/client";
 import { BrowserRouter } from "react-router-dom";
-import "./styles/Index.scss";
 import App from "./App";
+import ReactGA from "react-ga4";
+import "./styles/Index.scss";
 import "./i18n";
+
+ReactGA.initialize("G-BKHZCKPVK5");
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
--- src/pages/main/home/Home.js
+++ src/pages/main/home/Home.js
@@ -1,82 +1,82 @@
-import React, { useState } from "react";
+import React, { useState, useContext } from "react";
 import { useTranslation } from "react-i18next";
+import { LoadedContext } from "contexts/Context";
 import CustomButton from "components/customButton/CustomButton";
+import { ReactComponent as LoaderAnimation } from "assets/icons/Loader.svg";
 import { Link } from "react-scroll";
 import { ReactComponent as ArrowDownIcon } from "assets/icons/ArrowDown.svg";
-import ContactForm from "components/contactForm/contactForm";
 import Modal from "@mui/material/Modal";
 import "./Home.scss";
 
 const Home = () => {
-  const [open, setOpen] = useState(false);
-  const handleOpen = () => setOpen(true);
-  const handleClose = () => setOpen(false);
+    const { loaded, handleIframeLoad } = useContext(LoadedContext);
+    const [open, setOpen] = useState(false);
+    const handleOpen = () => setOpen(true);
+    const handleClose = () => setOpen(false);
 
-  const { t } = useTranslation();
+    const { t } = useTranslation();
 
-  return (
-    <div className="wrapper-home" name="home">
-      <div className="home-content-wrapper">
-        <div className="pre-title">{t("COMMUNICATIONS SOLUTIONS")}</div>
-        <div className="company-neme">
-          New World
-          <br />
-          Telecom
-        </div>
-        <div className="slogan">
-          Do what you can, with what you have, <br />
-          where you are.
-        </div>
-        <CustomButton label="get hiring" onClick={handleOpen} />
-        <Link className="link" activeClass="active" to="about" smooth spy offset={-59} duration={300}>
-          <div className="arrow-down">
-            <ArrowDownIcon />
-          </div>
-        </Link>
-      </div>
-      <Modal open={open} onClose={handleClose}>
-        <>
-          <div className="modal-content">
-            <div className="requirements">
-              <div className="requirements-wrapper">
-                <div className="requirements-title">
-                  <h4>{t("requirements individual_0")}</h4>
+    return (
+        <div
+            className="wrapper-home"
+            name="home"
+        >
+            <div className="home-content-wrapper">
+                <div className="pre-title">{t("COMMUNICATIONS SOLUTIONS")}</div>
+                <div className="company-neme">
+                    New World
+                    <br />
+                    Telecom
                 </div>
-                <div className="requirements-content">
-                  <h4>{t("requirements individual_1")}</h4>
-                  <ul>
-                    <li>{t("requirements individual_2")}</li>
-                    <li>{t("requirements individual_3")}</li>
-                    <li>{t("requirements individual_4")}</li>
-                    <li>{t("requirements individual_5")}</li>
-                    <li>{t("requirements individual_6")}</li>
-                  </ul>
+                <div className="slogan">
+                    Do what you can, with what you have, <br />
+                    where you are.
                 </div>
-              </div>
-              <div className="requirements-wrapper">
-                <div className="requirements-title">
-                  <h4>{t("requirements subcontractors_0")}</h4>
-                </div>
-                <div className="requirements-content">
-                  <h4>{t("requirements individual_1")}</h4>
-                  <ul>
-                    <li>{t("requirements subcontractors_2")}</li>
-                    <li>{t("requirements individual_3")}</li>
-                    <li>{t("requirements individual_4")}</li>
-                    <li>{t("requirements individual_5")}</li>
-                    <li>{t("requirements individual_6")}</li>
-                    <li>{t("requirements subcontractors_7")}</li>
-                  </ul>
-                </div>
-              </div>
+                <CustomButton
+                    label="get hiring"
+                    onClick={handleOpen}
+                />
+                <Link
+                    className="link"
+                    activeClass="active"
+                    to="about"
+                    smooth
+                    spy
+                    offset={-59}
+                    duration={300}
+                >
+                    <div className="arrow-down">
+                        <ArrowDownIcon />
+                    </div>
+                </Link>
             </div>
-            <ContactForm />
-          </div>
-          <div className="child" />
-        </>
-      </Modal>
-    </div>
-  );
+
+            <Modal
+                open={open}
+                onClose={handleClose}
+            >
+                <>
+                    <div className="modal-content">
+                        <div
+                            className="modal-close-button"
+                            onClick={handleClose}
+                        >
+                            🞪
+                        </div>
+                        <div className="employee-loader">{!loaded && <LoaderAnimation />}</div>
+                        <iframe
+                            src="https://forms.monday.com/forms/embed/841dbe78d20737008c792d3f09157e85?r=use1"
+                            width="650"
+                            height="500"
+                            title="Employee form"
+                            onLoad={handleIframeLoad}
+                        />
+                    </div>
+                    <div className="child" />
+                </>
+            </Modal>
+        </div>
+    );
 };
 
 export default Home;
